afe47abfb246ac039006bfc990589554d2f217f1
colons <code@colons.co>
2015-08-17 13:01:06 +0100

    Implement a date picker.

--- wfm.py
+++ wfm.py
@@ -1,14 +1,19 @@
-from __future__ import print_function, unicode_literals
+from __future__ import print_function, unicode_literals, division
 
+import datetime
 import os
 from sys import exit
 
 import colors
+from dateutil.parser import parse as parse_date
 from lxml import etree
 import requests
 from six.moves import input
 import yaml
 
+
+DAY_BUFFER = 14
+DATE_FORMAT = '%Y%m%d'
 
 CONFIG_TUTORIAL = """
 The config is a YAML file with values for 'email', 'apiKey', and 'accountKey'.
@@ -50,6 +55,16 @@ def input_valid(message, validate):
             print('try again')
 
 
+def strfdate(date):
+    return date.strftime(DATE_FORMAT)
+
+
+def strfmins(minutes):
+    hours = minutes // 60
+    remainder = minutes % 60
+    return '{}:{:02}'.format(hours, remainder)
+
+
 class WFMError(Exception):
     pass
 
@@ -58,6 +73,9 @@ class Client(object):
     base = 'https://api.workflowmax.com/{}'
     email = EMAIL
 
+    def __init__(self):
+        self.my_id = self._get_my_id()
+
     def request(self, method, path, **extra_params):
         params = {
             'apiKey': API_KEY,
@@ -65,8 +83,11 @@ class Client(object):
         }
         params.update(extra_params)
         resp = requests.request(method, self.base.format(path), params=params)
+
         if resp.status_code != 200:
-            raise ValueError('non-200 response: {}'.format(resp.status_code))
+            raise WFMError('{}\n\nnon-200 response: {}'.format(
+                resp.status_code, resp.content))
+
         try:
             parsed = etree.fromstring(resp.content)
         except etree.XMLSyntaxError:
@@ -81,7 +102,7 @@ class Client(object):
 
         return parsed
 
-    def get_my_id(self):
+    def _get_my_id(self):
         for staff in self.request('get', 'staff.api/list').find('StaffList'):
             if staff.find('Email').text == self.email:
                 return staff.find('ID').text
@@ -90,10 +111,9 @@ class Client(object):
                          .format(self.email))
 
     def get_my_jobs(self):
-        my_id = client.get_my_id()
         return [
             job for job in self.request('get', 'job.api/current').find('Jobs')
-            if my_id in [s.find('ID').text for s in job.find('Assigned')]
+            if self.my_id in [s.find('ID').text for s in job.find('Assigned')]
         ]
 
     def get_tasks_for_job(self, job_id):
@@ -101,9 +121,21 @@ class Client(object):
             'get', 'job.api/get/{}'.format(job_id)
         ).find('Job').find('Tasks')
 
+    def get_my_recent_times(self):
+        return [
+            t for t in client.request('get', 'time.api/list', **{
+                'from': strfdate(datetime.date.today() -
+                                 datetime.timedelta(days=DAY_BUFFER)),
+                'to': strfdate(datetime.date.today() +
+                               datetime.timedelta(days=1)),
+            }).find('Times')
+            if t.find('Staff').find('ID').text == self.my_id
+        ]
+
 
 if __name__ == '__main__':
     client = Client()
+
     jobs = sorted(
         client.get_my_jobs(),
         key=lambda j: (
@@ -113,7 +145,7 @@ if __name__ == '__main__':
     )
 
     for i, job in enumerate(jobs):
-        print('{index}: {client} | {job}'.format(
+        print('{index}: {job} | {client}'.format(
             index=colors.bold('{:3}'.format(i+1)),
             client=colors.cyan(job.find('Client').find('Name').text.strip()),
             job=colors.magenta(job.find('Name').text.strip()),
@@ -140,3 +172,32 @@ if __name__ == '__main__':
             '\npick a task (1-{}): '.format(len(tasks)),
             lambda i: tasks[int(i)-1],
         )
+
+    times = client.get_my_recent_times()
+    today = datetime.date.today()
+    calendar = []
+    for days_ago in range(DAY_BUFFER, -1, -1):
+        target_date = today - datetime.timedelta(days=days_ago)
+        total_minutes = 0
+        for time in times:
+            if parse_date(time.find('Date').text).date() == target_date:
+                total_minutes += int(time.find('Minutes').text)
+        calendar.append((target_date, total_minutes))
+
+    print()
+
+    for i, (date, minutes) in enumerate(calendar):
+        weekday = date.weekday() not in (5, 6)
+        bold_if_weekday = colors.bold if weekday else lambda s: s
+        minute_coloration = colors.green if minutes else colors.red
+        print('{index}: {time} {weekday} {date}'.format(
+            index=colors.bold('{:3}'.format(i+1)),
+            weekday='-' if weekday else ' ',
+            date=colors.yellow(date.strftime('%a %b %d')),
+            time=bold_if_weekday(minute_coloration(strfmins(minutes))),
+        ))
+
+    date = input_valid(
+        '\npick a day (1-{0}, default {0}): '.format(len(calendar)),
+        lambda i: calendar[(int(i) if i else len(calendar))-1][1],
+    )
